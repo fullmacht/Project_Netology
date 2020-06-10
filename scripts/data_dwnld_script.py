b5a0b01a5c5be3c2a9fd9ce64215e1046ed48cd2
laptop <fany012@yandex.ru>
2020-06-10 20:01:46 +0300

    1.1

---
+++ scripts/data_dwnld_script.py
@@ -0,0 +1,11 @@
+import yadisk
+import os
+
+token = 'AgAAAAAmaJOdAAZk1uxHxyasKkedh5gkVD-fJsE'
+y = yadisk.YaDisk(token=token)
+te = os.path.dirname(os.getcwd()[0:-7]) + r'\data\processed\test.csv'
+tr = os.path.dirname(os.getcwd()[0:-7]) + r'\data\processed\train.csv'
+s = os.path.dirname(os.getcwd()[0:-7]) + r'\data\external\sample_submission.csv'
+y.download("/Netology/test.csv", te)
+y.download("/Netology/train.csv", tr)
+y.download("/Netology/sample_submission.csv", s)
